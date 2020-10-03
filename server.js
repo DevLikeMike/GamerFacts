fc55a354a0b8b48fd8031c697c3aba43a389603b
DevLikeMike <michael@MacBook-Pro.lan1>
2020-10-03 14:35:47 -0400

    Prepare Deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -9,16 +10,20 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({
-    msg: "Welcome to the contact api",
-  });
-});
-
 //Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/games", require("./routes/games"));
+
+//Serve Static assets
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
